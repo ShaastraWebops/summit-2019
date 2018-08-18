89ab5722b47aa947d9fa1f2eae7bea0a599283ef
gyash24x7 <gyash24x7@gmail.com>
2018-08-18 14:45:21 +0530

    added port variable

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 var app = express();
-
+var port = process.env.PORT || 8080
 app.use(express.static(__dirname + "/public"));
 
 app.get("*", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log("====================================");
   console.log("Listening on port 8080");
   console.log("====================================");
