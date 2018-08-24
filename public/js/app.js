a4ea1c31ee70a52916b0b1b4e36216b762ac17b2
Yash Gupta <gyash24x7@gmail.com>
2018-08-24 23:13:40 +0530

    added images and necessary changes done

--- public/js/app.js
+++ public/js/app.js
@@ -12,48 +12,48 @@
 	function NavBarDirective() {
 		return {
 			templateUrl: 'views/navbar.html'
-		}
+		};
 	}
 
 	function HomeDirective() {
 		return {
 			templateUrl: 'views/home.html'
-		}
+		};
 	}
 
 	function AboutDirective() {
 		return {
 			templateUrl: 'views/about.html'
-		}
+		};
 	}
 
 	function EditionsDirective() {
 		return {
 			templateUrl: 'views/editions.html'
-		}
+		};
 	}
 
 	function SummitDirective() {
 		return {
 			templateUrl: 'views/summit.html'
-		}
+		};
 	}
 
 	function RegistrationsDirective() {
 		return {
 			templateUrl: 'views/registrations.html'
-		}
+		};
 	}
 
 	function ContactDirective() {
 		return {
 			templateUrl: 'views/contact.html'
-		}
+		};
 	}
 
 	function FooterDirective(){
 		return {
 			templateUrl: '/views/footer.html'
-		}
+		};
 	}
 })();
